1ef2a07ecd355e6e4d10246fd64e4f3ccd3ee2e9
jw1026850952 <66473407+jw1026850952@users.noreply.github.com>
2022-09-28 16:48:29 +0800

    Initial commit

---
+++ dm-common/dm-common-client/src/test/java/cn/dm/DmCommonClientApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.dm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmCommonClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
